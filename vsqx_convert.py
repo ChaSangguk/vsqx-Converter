f8535052289f54cc806d72c6e920285d598c0bc3
chasangguk <68228980+ChaSangguk@users.noreply.github.com>
2026-07-07 10:10:08 +0900

    필요없는 코드 삭제

--- vsqx_convert.py
+++ vsqx_convert.py
@@ -1,9 +1,9 @@
-import json
-from lxml import etree
+from lxml import etree # type: ignore
 '''
 vsqx 파일 내 vstrack, vspart, note내 <p> 값을 파싱하여 미리 준비된 리스트에 따라 변환후 저장
 '''
 class VsqxConverter:
+    NS = {"v4": "http://www.yamaha.co.jp/vocaloid/schema/vsq4/"}
     def __init__(self, vsqx_file = None, convert_file = None):
         self.vsqx_file = vsqx_file
         self.convert_file = convert_file
@@ -15,8 +15,8 @@ class VsqxConverter:
         assert vsqx_file is not None, "vsqx_file is required"
         assert convert_file is not None, "convert_file is required"
         root = vsqx_file.getroot()
-        ns = {"v4": "http://www.yamaha.co.jp/vocaloid/schema/vsq4/"}
-        for note in root.findall(".//v4:note/v4:p", namespaces=ns):
+        
+        for note in root.findall(".//v4:note/v4:p", namespaces=VsqxConverter.NS):
             origin_note = note.text
             result = []
             if origin_note is None:
@@ -29,8 +29,3 @@ class VsqxConverter:
             note.set("lock", "1")
             note.text = etree.CDATA(' '.join(result))
         return etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
-        '''
-        with open(self.vsqx_file.replace('.vsqx', '_updated.vsqx'), "wb") as f:
-            f.write(etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True))
-        '''
-
