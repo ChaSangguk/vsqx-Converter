7423a28a40053156c7473006d20604214b2986d4
chasangguk <68228980+ChaSangguk@users.noreply.github.com>
2026-08-03 11:03:22 +0900

    namespace 사용 개선 및 변환 로직 최적화

--- vsqx_convert.py
+++ vsqx_convert.py
@@ -7,11 +7,10 @@ vsqx 파일 내 vstrack, vspart, note내 <p> 값을 파싱하여 미리 준비�
 ''' 
 todo :
 update 2026-07-29
-    1. [ ] 파일 종류에 따라 namespace를 범용적으로 사용가능하게 수정
+    1. [X] 파일 종류에 따라 namespace를 범용적으로 사용가능하게 수정
     2. [ ] 단위 테스트 추가
 '''
 class VsqxConverter:
-    NS: Dict[str, str] = {"v4": "http://www.yamaha.co.jp/vocaloid/schema/vsq4/"}
     vsqx_file: etree._ElementTree | None
     convert_file: Dict[str, Dict[str, str]] | None
 
@@ -25,26 +24,31 @@ class VsqxConverter:
         if convert_file is None:
             convert_file = self.convert_file
 
-        
         assert vsqx_file is not None, "vsqx_file is required"
         assert convert_file is not None, "convert_file is required"
 
         root: etree._Element = vsqx_file.getroot()
-        ns_uri = root.nsmap
-        for note in root.findall(".//v4:note/v4:p", namespaces=VsqxConverter.NS):
-            origin_note: str | None = note.text
-            if origin_note is None:
-                continue
-            result: list[str] = []
-            for i in convert_file["multiple"]:
-                if i not in origin_note:
+        ns_uri: Dict[str, str] = root.nsmap
+        for (NS, uri) in ns_uri.items():
+            if NS is None:
+                path = ".//ns0:note/ns0:p"
+                NS = "ns0"
+            else:
+                path = f".//{NS}:note/{NS}:p"
+            for note in root.findall(path, namespaces={NS : uri}):
+                origin_note: str | None = note.text
+                if origin_note is None:
                     continue
-                origin_note = origin_note.replace(i, convert_file["multiple"][i])
-            for n in origin_note.split():
-                if n in convert_file["singular"]:
-                    result.append(convert_file["singular"][n])
-                else:
-                    result.append(n)
-            note.set("lock", "1")
-            note.text = etree.CDATA(" ".join(result))
+                result: list[str] = []
+                for i in convert_file["multiple"]:
+                    if i not in origin_note:
+                        continue
+                    origin_note = origin_note.replace(i, convert_file["multiple"][i])
+                for n in origin_note.split():
+                    if n in convert_file["singular"]:
+                        result.append(convert_file["singular"][n])
+                    else:
+                        result.append(n)
+                note.set("lock", "1")
+                note.text = etree.CDATA(" ".join(result))
         return etree.tostring(root, pretty_print=True, encoding="utf-8", xml_declaration=True)
